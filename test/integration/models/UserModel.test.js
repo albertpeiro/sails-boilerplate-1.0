523bf07791f854a0370f06f97deca8f1d6d460e0
Albert Peiró <acordonet@gmail.com>
2017-06-25 20:51:22 +0200

    Add testing suite

---
+++ test/integration/models/UserModel.test.js
@@ -0,0 +1,13 @@
+// ./test/integration/models/User.test.js
+
+var util = require('util');
+
+describe('User (model)', function() {
+
+  describe('#findBestStudents()', function() {
+    it('should return 5 users', function (done) {
+      return done();
+    });
+  });
+
+});
